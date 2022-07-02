443fc18216e51d4ceeaf2e60e869f1923c561900
lardira <danilkefimov@gmail.com>
2022-07-02 21:04:24 +0300

    removed debug code

--- note-tracker/note-tracker.js
+++ note-tracker/note-tracker.js
@@ -16,16 +16,3 @@ const AddNote = (note) => {
     }
     console.log('Note was saved successfully!');
 }
-
-const testNote = {
-    id: 0,
-    title: 'Note',
-    text: 'buy some food tomorrow'
-}
-
-for(let i = 0; i < 40; i++)
-{
-    testNote.id = i;
-    testNote.title = `Note #${i + 1}`;
-    AddNote(JSON.stringify(testNote));
-}
